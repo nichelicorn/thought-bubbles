a6b9a3c229a050c0b3e3c1c92ddaee45dcd5f52a
Nichele Dunn <the7yearitch@hotmail.com>
2021-12-20 12:02:05 -0700

    ✨ Add Ideas component

---
+++ src/Ideas.js
@@ -0,0 +1,12 @@
+// This is the Ideas component, where Idea cards will be displayed
+
+import React from "react";
+import "./Ideas.css";
+
+const Ideas = () => {
+  return (
+    <h2>Ideas will be displayed here!</h2>
+  )
+}
+
+export default Ideas;
