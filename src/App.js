757edd4d4bc108bdc837e5210615b6db2a071637
Nichele Dunn <the7yearitch@hotmail.com>
2021-12-20 15:32:06 -0700

    📝 Add some notes and play around with data types

--- src/App.js
+++ src/App.js
@@ -12,23 +12,24 @@ class App extends Component {
     // this, here, is binding the state / relevant data application, to this component
     // Calvin and Hobbes quotes from: https://www.gocomics.com/comics/lists/1643203/calvin-and-hobbes-bus-stop-musings
     this.state = {
-      ideas: [
-        {
-          id: 1,
-          title: "Hobbes says: (adding two spikes to Calvin's hair)",
-          description: "Trust me. You look like ... like ... Astro Boy.",
-        },
-        {
-          id: 2,
-          title: "Calvin says (standing in the rain):",
-          description: "Why in the world am I waiting in the pouring fain for the school bus to take me somewhere I don't even want to go? ... I go to school, but I never learn what I want to know.",
-        },
-        {
-          id: 3,
-          title: "Hobbes says, to Calvin:",
-          description: "How on Earth did you get all the say to the bus stop with both feet through one pant leg?",
-        }
-      ]
+      // ideas: [
+      //   {
+      //     id: 1,
+      //     title: "Hobbes says: (adding two spikes to Calvin's hair)",
+      //     description: "Trust me. You look like ... like ... Astro Boy.",
+      //   },
+      //   {
+      //     id: 2,
+      //     title: "Calvin says (standing in the rain):",
+      //     description: "Why in the world am I waiting in the pouring fain for the school bus to take me somewhere I don't even want to go? ... I go to school, but I never learn what I want to know.",
+      //   },
+      //   {
+      //     id: 3,
+      //     title: "Hobbes says, to Calvin:",
+      //     description: "How on Earth did you get all the say to the bus stop with both feet through one pant leg?",
+      //   }
+      // ]
+      ideas: []
     }
   }
 
--- src/Ideas.js
+++ src/Ideas.js
@@ -1,10 +1,20 @@
 // This is the Ideas component, where Idea cards will be displayed
 
-import React from "react";
+import React, { StrictMode } from "react";
 import Card from "./Card.js"
 import "./Ideas.css";
 
 const Ideas = ( {ideas} ) => {
+
+  // 👇 this was kind of interesting ... the ideas are an object, even though they are an Array; might read into this later
+  // console.log("Ideas component ideas", ideas); // logs Array []
+  // console.log("typeof ideas", typeof ideas); // logs object
+
+  if (! ideas.length) {
+    return (
+      <h3>Hey! Add some thinky thinks!</h3>
+    )
+  }
 
   // Test code for looking at the way props interact between parent / child components
   // const { name, blade } = props;
