7377699244f3ef6d74fb1329ecd73a324f9acedc
Nichele Dunn <the7yearitch@hotmail.com>
2021-12-29 19:30:55 -0700

    ♻️  Work on localStorage

--- src/App.js
+++ src/App.js
@@ -27,24 +27,26 @@ class App extends Component {
     this.setState({ ideas: filteredIdeas });
   }
 
-  setStorage = () => {
-    console.log("something is happening! 🧐 ");
-    const storedIdeas = this.state.ideas; // this is logging state before the new idea is added to the array; move this logic into the Form component to keep the timing more consistent?
-    console.log("ideas to store <>>>", storedIdeas);
+  setStorage = () => { // localStorage is being reset to an empty array each time the page is refreshed
+    // console.log("something is happening! 🧐 ");
+    const storedIdeas = JSON.stringify(this.state.ideas);
+    console.log("storedIdeas <>>>", storedIdeas);
     localStorage.setItem("ideas", storedIdeas);
-    console.log("this.state.ideas <>>>", this.state.ideas);
   }
+
+  // getStored = () => {
+  //   const retrievedIdeas = JSON.parse(localStorage.getItem("ideas"));
+  //   console.log("retrievedIdeas <>>>", retrievedIdeas); // logs previously stored array
+  //   this.setState({ ideas: retrievedIdeas });
+  // }
+
+  getStored = () => { JSON.parse(localStorage.getItem("ideas")) }
 
   render() {
     return (
       <main className="App">
         <h1>Thought Bubbles</h1>
         <Form addIdea={this.addIdea} />
-        {/* Test code for looking at the way props interact between parent / child components */}
-        {/* <Ideas name="Ahsoka" blade="green"/>
-        <Ideas name="Anakin" blade="blue"/>
-        <Ideas name="Mace" blade="purple" /> */}
-        {/* Conditional rendering based on `this.state` */}
         {!this.state.ideas.length && <h2>Hey! Add some thoughts to make bubbles 🧼</h2>}
         <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea} setStorage={this.setStorage} />
       </main>
--- src/Ideas.js
+++ src/Ideas.js
@@ -6,20 +6,6 @@ import "./Ideas.css";
 
 const Ideas = ( { ideas, deleteIdea, setStorage } ) => {
 
-  // 👇 this was kind of interesting ... the ideas are an object, even though they are an Array; might read into this later
-  // console.log("Ideas component ideas", ideas); // logs Array []
-  // console.log("typeof ideas", typeof ideas); // logs object
-
-  // Conditional rendering using JS in the component
-  // if (! ideas.length) {
-  //   return (
-  //     <h2>Hey! Add some thinky thinks!</h2>
-  //   )
-  // }
-
-  // Test code for looking at the way props interact between parent / child components
-  // const { name, blade } = props;
-
   const ideaCards = ideas.map(idea => {
     return (
       <Card 
@@ -34,6 +20,7 @@ const Ideas = ( { ideas, deleteIdea, setStorage } ) => {
 
   return (
     <div className="ideas-wrap">
+      {console.log("ideas <>>>", ideas)}
       {ideaCards}
       {setStorage()}
     </div>
